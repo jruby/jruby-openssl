cb28b5b148a9795960dae96d846af2edcb1d19c9
kares <self@kares.org>
2015-05-21 13:42:45 +0200

    make sure we route to jopenssl22 under 9K for MRI compatible openssl/xxx requires

--- lib/openssl/x509-internal.rb
+++ lib/openssl/x509-internal.rb
@@ -1,7 +1,9 @@
-if RUBY_VERSION >= '2.1.0'
+if RUBY_VERSION > '2.2'
+  raise LoadError, "no such library in 2.2: openssl/x509-internal.rb"
+elsif RUBY_VERSION > '2.1'
   raise LoadError, "no such library in 2.1: openssl/x509-internal.rb"
-elsif RUBY_VERSION >= '1.9.0'
-  load('jopenssl19/openssl/x509-internal.rb')
+elsif RUBY_VERSION > '1.9'
+  load "jopenssl19/openssl/#{File.basename(__FILE__)}"
 else
-  load('jopenssl18/openssl/x509-internal.rb')
+  load "jopenssl18/openssl/#{File.basename(__FILE__)}"
 end
