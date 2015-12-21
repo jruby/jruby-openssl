4776d35d19c0e3a4d8f4507051ccb5c94f47af10
kares <self@kares.org>
2015-12-21 12:16:02 +0100

    preparing for 0.9.13 + start exposing VERSION constant(s) directly on Jopenssl module

--- lib/jopenssl/version.rb
+++ lib/jopenssl/version.rb
@@ -1,6 +1,11 @@
 module Jopenssl
+  VERSION = '0.9.13'
+  BOUNCY_CASTLE_VERSION = '1.50'
+  # @deprecated
   module Version
-    VERSION = '0.9.13.dev'
-    BOUNCY_CASTLE_VERSION = '1.50'
+    # @private
+    VERSION = Jopenssl::VERSION
+    # @private
+    BOUNCY_CASTLE_VERSION = Jopenssl::BOUNCY_CASTLE_VERSION
   end
 end
