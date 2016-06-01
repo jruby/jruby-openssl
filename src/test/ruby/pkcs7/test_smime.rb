eee8f36b1daa9b86e7390138aa65a2b7879ea63c
kares <self@kares.org>
2016-06-01 12:19:08 +0200

    [test] fix 9.1 compatibility - no longer a new on Java interfaces

--- src/test/ruby/pkcs7/test_smime.rb
+++ src/test/ruby/pkcs7/test_smime.rb
@@ -4,8 +4,7 @@ module PKCS7Test
   class TestSMIME < TestCase
     def test_read_pkcs7_should_raise_error_when_parsing_headers_fails
       bio = BIO.new
-      mime = Mime.new
-      mime.stubs(:parseHeaders).returns(nil)
+      mime = Mime.impl { |name, *args| name == :parseHeaders ? nil : raise }
 
       begin
         SMIME.new(mime).readPKCS7(bio, nil)
@@ -19,7 +18,7 @@ module PKCS7Test
 
     def test_read_pkcs7_should_raise_error_when_content_type_is_not_there
       bio = BIO.new
-      mime = Mime.new
+      mime = Mime.impl {}
 
       headers = ArrayList.new
       mime.expects(:parseHeaders).with(bio).returns(headers)
@@ -34,7 +33,7 @@ module PKCS7Test
         assert_equal PKCS7::R_NO_CONTENT_TYPE, e.get_reason
       end
 
-      mime = Mime.new
+      mime = Mime.impl {}
       mime.expects(:parseHeaders).with(bio).returns(headers)
       mime.expects(:findHeader).with(headers, "content-type").returns(MimeHeader.new("content-type", nil))
 
@@ -49,20 +48,18 @@ module PKCS7Test
     end
 
     def test_read_pkcs7_should_set_the_second_arguments_contents_to_null_if_its_there
-      mime = Mime.new
-      mime.stubs(:parseHeaders).raises("getOutOfJailForFree")
+      mime = Mime.impl { |name, *args| name == :parseHeaders ? raise("parseHeaders") : raise }
 
       bio2 = BIO.new
       arr = [bio2].to_java BIO
 
       begin
         SMIME.new(mime).readPKCS7(nil, arr)
-      rescue
+      rescue => e
+        assert_equal 'parseHeaders', e.message
       end
 
       assert_nil arr[0]
-
-
       arr = [bio2, bio2].to_java BIO
       begin
         SMIME.new(mime).readPKCS7(nil, arr)
@@ -75,11 +72,18 @@ module PKCS7Test
 
     def test_read_pkcs7_should_call_methods_on_mime
       bio = BIO.new
-      mime = Mime.new
 
-      headers = ArrayList.new
-      mime.expects(:parseHeaders).with(bio).returns(headers)
-      mime.expects(:findHeader).with(headers, "content-type").returns(MimeHeader.new("content-type", "application/pkcs7-mime"))
+      mime = Mime.impl do |name, *args|
+        case name
+        when :parseHeaders then ArrayList.new
+        when :findHeader then
+          if args[1] == 'content-type'
+            MimeHeader.new(args[1], "application/pkcs7-mime")
+          else
+            raise args.inspect
+          end
+        end
+      end
 
       begin
         SMIME.new(mime).readPKCS7(bio, nil)
@@ -90,11 +94,17 @@ module PKCS7Test
 
     def test_read_pkcs7_throws_correct_exception_if_wrong_content_type
       bio = BIO.new
-      mime = Mime.new
-
-      headers = ArrayList.new
-      mime.expects(:parseHeaders).with(bio).returns(headers)
-      mime.expects(:findHeader).with(headers, "content-type").returns(MimeHeader.new("content-type", "foo"))
+      mime = Mime.impl do |name, *args|
+        case name
+        when :parseHeaders then ArrayList.new
+        when :findHeader then
+          if args[1] == 'content-type'
+            MimeHeader.new(args[1], "foo")
+          else
+            raise args.inspect
+          end
+        end
+      end
 
       begin
         SMIME.new(mime).readPKCS7(bio, nil)
@@ -109,13 +119,19 @@ module PKCS7Test
 
     def test_read_pkcs7_with_multipart_should_fail_if_no_boundary_found
       bio = BIO.new
-      mime = Mime.new
-
-      headers = ArrayList.new
       hdr = MimeHeader.new("content-type", "multipart/signed")
-      mime.expects(:parseHeaders).with(bio).returns(headers)
-      mime.expects(:findHeader).with(headers, "content-type").returns(hdr)
-
+      mime = Mime.impl do |name, *args|
+        case name
+        when :parseHeaders then ArrayList.new
+        when :findHeader then
+          if args[1] == 'content-type'
+            hdr
+          else
+            raise args.inspect
+          end
+        end
+      end
+      hdr = MimeHeader.new("content-type", "multipart/signed")
       mime.expects(:findParam).with(hdr, "boundary").returns(nil)
 
       begin
@@ -130,7 +146,7 @@ module PKCS7Test
 
     def test_read_pkcs7_with_multipart_should_fail_if_null_boundary_value
       bio = BIO.new
-      mime = Mime.new
+      mime = Mime.impl {}
 
       headers = ArrayList.new
       hdr = MimeHeader.new("content-type", "multipart/signed")
@@ -152,7 +168,7 @@ module PKCS7Test
     # TODO: redo this test to be an integration test
     def _test_read_pkcs7_happy_path_without_multipart
       bio = BIO.new
-      mime = Mime.new
+      mime = Mime.impl {}
 
       headers = ArrayList.new
       mime.expects(:parseHeaders).with(bio).returns(headers)
@@ -164,19 +180,20 @@ module PKCS7Test
     def test_read_pkcs7_happy_path_multipart
       bio = BIO::from_string(MultipartSignedString)
       mime = Mime::DEFAULT
-      p7 = SMIME.new(mime).readPKCS7(bio, nil)
+      SMIME.new(mime).readPKCS7(bio, nil)
     end
 
     def test_read_pkcs7_happy_path_without_multipart_enveloped
       bio = BIO::from_string(MimeEnvelopedString)
       mime = Mime::DEFAULT
-      p7 = SMIME.new(mime).readPKCS7(bio, nil)
+      SMIME.new(mime).readPKCS7(bio, nil)
     end
 
     def test_read_pkcs7_happy_path_without_multipart_signed
       bio = BIO::from_string(MimeSignedString)
       mime = Mime::DEFAULT
-      p7 = SMIME.new(mime).readPKCS7(bio, nil)
+      SMIME.new(mime).readPKCS7(bio, nil)
     end
+
   end
 end
