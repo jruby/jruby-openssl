66b4da35d037269397c94fda03410bbcdd34b8b7
kares <self@kares.org>
2017-10-29 16:56:55 +0100

    [refactor] work-around on BN#to_s impl which has been due JRuby 1.6

--- src/main/java/org/jruby/ext/openssl/BN.java
+++ src/main/java/org/jruby/ext/openssl/BN.java
@@ -178,22 +178,20 @@ public class BN extends RubyObject {
         return this;
     }
 
-    @JRubyMethod(name = "to_s", rest = true, optional = 1)
+    @Deprecated
     public RubyString to_s(IRubyObject[] args) {
         int argc = Arity.checkArgumentCount(getRuntime(), args, 0, 1);
         return to_s( argc == 1 ? RubyNumeric.num2int(args[0]) : 10 );
     }
 
-    // 1.6.8 can not handle - this way :
-
     @Override
-    //@JRubyMethod(name = "to_s")
+    @JRubyMethod(name = "to_s")
     public RubyString to_s() { return to_s(10); }
 
-    //@JRubyMethod(name = "to_s")
-    //public RubyString to_s(IRubyObject base) {
-    //    return to_s( RubyNumeric.num2int(base) );
-    //}
+    @JRubyMethod(name = "to_s")
+    public RubyString to_s(IRubyObject base) {
+        return to_s( RubyNumeric.num2int(base) );
+    }
 
     private RubyString to_s(final int base) {
         final Ruby runtime = getRuntime();
