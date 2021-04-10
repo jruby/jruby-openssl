bd845e6c8c9dbfefc1e5b501545d23d8c95bf810
kares <self@kares.org>
2021-04-10 16:26:29 +0200

    [compat] align with Ruby OpenSSL 2.2.0

--- lib/jopenssl23/openssl/cipher.rb
+++ lib/jopenssl23/openssl/cipher.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 #--
 # = Ruby-space predefined Cipher subclasses
 #
@@ -14,6 +14,48 @@
 
 module OpenSSL
   class Cipher
+    # %w(AES CAST5 BF DES IDEA RC2 RC4 RC5).each{|name|
+    #   klass = Class.new(Cipher){
+    #     define_method(:initialize){|*args|
+    #       cipher_name = args.inject(name){|n, arg| "#{n}-#{arg}" }
+    #       super(cipher_name.downcase)
+    #     }
+    #   }
+    #   const_set(name, klass)
+    # }
+    #
+    # %w(128 192 256).each{|keylen|
+    #   klass = Class.new(Cipher){
+    #     define_method(:initialize){|mode = "CBC"|
+    #       super("aes-#{keylen}-#{mode}".downcase)
+    #     }
+    #   }
+    #   const_set("AES#{keylen}", klass)
+    # }
+
+    # call-seq:
+    #   cipher.random_key -> key
+    #
+    # Generate a random key with OpenSSL::Random.random_bytes and sets it to
+    # the cipher, and returns it.
+    #
+    # You must call #encrypt or #decrypt before calling this method.
+    # def random_key
+    #   str = OpenSSL::Random.random_bytes(self.key_len)
+    #   self.key = str
+    # end
+
+    # call-seq:
+    #   cipher.random_iv -> iv
+    #
+    # Generate a random IV with OpenSSL::Random.random_bytes and sets it to the
+    # cipher, and returns it.
+    #
+    # You must call #encrypt or #decrypt before calling this method.
+    # def random_iv
+    #   str = OpenSSL::Random.random_bytes(self.iv_len)
+    #   self.iv = str
+    # end
 
     # Deprecated.
     #
