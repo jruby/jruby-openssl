b86c549cd5f90719a865ed5e3be03cb6f9812775
kares <self@kares.org>
2021-10-14 13:38:53 +0200

    [refactor] reviewed message mappings

--- src/main/java/org/jruby/ext/openssl/x509store/X509Utils.java
+++ src/main/java/org/jruby/ext/openssl/x509store/X509Utils.java
@@ -105,93 +105,94 @@ public abstract class X509Utils {
      */
     public static String verifyCertificateErrorString(final int error) {
         switch (error) {
-        case V_OK: return("ok");
-        case V_ERR_UNABLE_TO_GET_ISSUER_CERT:
+            case V_OK:
+                return("ok");
+            case V_ERR_UNABLE_TO_GET_ISSUER_CERT:
                 return("unable to get issuer certificate");
-        case V_ERR_UNABLE_TO_GET_CRL:
+            case V_ERR_UNABLE_TO_GET_CRL:
                 return("unable to get certificate CRL");
-        case V_ERR_UNABLE_TO_DECRYPT_CERT_SIGNATURE:
+            case V_ERR_UNABLE_TO_DECRYPT_CERT_SIGNATURE:
                 return("unable to decrypt certificate's signature");
-        case V_ERR_UNABLE_TO_DECRYPT_CRL_SIGNATURE:
+            case V_ERR_UNABLE_TO_DECRYPT_CRL_SIGNATURE:
                 return("unable to decrypt CRL's signature");
-        case V_ERR_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY:
+            case V_ERR_UNABLE_TO_DECODE_ISSUER_PUBLIC_KEY:
                 return("unable to decode issuer public key");
-        case V_ERR_CERT_SIGNATURE_FAILURE:
+            case V_ERR_CERT_SIGNATURE_FAILURE:
                 return("certificate signature failure");
-        case V_ERR_CRL_SIGNATURE_FAILURE:
+            case V_ERR_CRL_SIGNATURE_FAILURE:
                 return("CRL signature failure");
-        case V_ERR_CERT_NOT_YET_VALID:
+            case V_ERR_CERT_NOT_YET_VALID:
                 return("certificate is not yet valid");
-        case V_ERR_CRL_NOT_YET_VALID:
+            case V_ERR_CRL_NOT_YET_VALID:
                 return("CRL is not yet valid");
-        case V_ERR_CERT_HAS_EXPIRED:
+            case V_ERR_CERT_HAS_EXPIRED:
                 return("certificate has expired");
-        case V_ERR_CRL_HAS_EXPIRED:
+            case V_ERR_CRL_HAS_EXPIRED:
                 return("CRL has expired");
-        case V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD:
+            case V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD:
                 return("format error in certificate's notBefore field");
-        case V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD:
+            case V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD:
                 return("format error in certificate's notAfter field");
-        case V_ERR_ERROR_IN_CRL_LAST_UPDATE_FIELD:
+            case V_ERR_ERROR_IN_CRL_LAST_UPDATE_FIELD:
                 return("format error in CRL's lastUpdate field");
-        case V_ERR_ERROR_IN_CRL_NEXT_UPDATE_FIELD:
+            case V_ERR_ERROR_IN_CRL_NEXT_UPDATE_FIELD:
                 return("format error in CRL's nextUpdate field");
-        case V_ERR_OUT_OF_MEM:
+            case V_ERR_OUT_OF_MEM:
                 return("out of memory");
-        case V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT:
+            case V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT:
                 return("self signed certificate");
-        case V_ERR_SELF_SIGNED_CERT_IN_CHAIN:
+            case V_ERR_SELF_SIGNED_CERT_IN_CHAIN:
                 return("self signed certificate in certificate chain");
-        case V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY:
+            case V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY:
                 return("unable to get local issuer certificate");
-        case V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE:
+            case V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE:
                 return("unable to verify the first certificate");
-        case V_ERR_CERT_CHAIN_TOO_LONG:
+            case V_ERR_CERT_CHAIN_TOO_LONG:
                 return("certificate chain too long");
-        case V_ERR_CERT_REVOKED:
+            case V_ERR_CERT_REVOKED:
                 return("certificate revoked");
-        case V_ERR_INVALID_CA:
+            case V_ERR_INVALID_CA:
                 return ("invalid CA certificate");
-        case V_ERR_INVALID_NON_CA:
-                return ("invalid non-CA certificate (has CA markings)");
-        case V_ERR_PATH_LENGTH_EXCEEDED:
+            case V_ERR_PATH_LENGTH_EXCEEDED:
                 return ("path length constraint exceeded");
-        case V_ERR_PROXY_PATH_LENGTH_EXCEEDED:
-                return("proxy path length constraint exceeded");
-        case V_ERR_PROXY_CERTIFICATES_NOT_ALLOWED:
-                return("proxy cerificates not allowed, please set the appropriate flag");
-        case V_ERR_INVALID_PURPOSE:
+            case V_ERR_INVALID_PURPOSE:
                 return ("unsupported certificate purpose");
-        case V_ERR_CERT_UNTRUSTED:
+            case V_ERR_CERT_UNTRUSTED:
                 return ("certificate not trusted");
-        case V_ERR_CERT_REJECTED:
+            case V_ERR_CERT_REJECTED:
                 return ("certificate rejected");
-        case V_ERR_APPLICATION_VERIFICATION:
-                return("application verification failure");
-        case V_ERR_SUBJECT_ISSUER_MISMATCH:
+            case V_ERR_SUBJECT_ISSUER_MISMATCH:
                 return("subject issuer mismatch");
-        case V_ERR_AKID_SKID_MISMATCH:
+            case V_ERR_AKID_SKID_MISMATCH:
                 return("authority and subject key identifier mismatch");
-        case V_ERR_AKID_ISSUER_SERIAL_MISMATCH:
+            case V_ERR_AKID_ISSUER_SERIAL_MISMATCH:
                 return("authority and issuer serial number mismatch");
-        case V_ERR_KEYUSAGE_NO_CERTSIGN:
+            case V_ERR_KEYUSAGE_NO_CERTSIGN:
                 return("key usage does not include certificate signing");
-        case V_ERR_UNABLE_TO_GET_CRL_ISSUER:
+            case V_ERR_UNABLE_TO_GET_CRL_ISSUER:
                 return("unable to get CRL issuer certificate");
-        case V_ERR_UNHANDLED_CRITICAL_EXTENSION:
+            case V_ERR_UNHANDLED_CRITICAL_EXTENSION:
                 return("unhandled critical extension");
-        case V_ERR_KEYUSAGE_NO_CRL_SIGN:
+            case V_ERR_KEYUSAGE_NO_CRL_SIGN:
                 return("key usage does not include CRL signing");
-        case V_ERR_KEYUSAGE_NO_DIGITAL_SIGNATURE:
-                return("key usage does not include digital signature");
-        case V_ERR_UNHANDLED_CRITICAL_CRL_EXTENSION:
+            case V_ERR_UNHANDLED_CRITICAL_CRL_EXTENSION:
                 return("unhandled critical CRL extension");
-        case V_ERR_INVALID_EXTENSION:
+            case V_ERR_INVALID_NON_CA:
+                return ("invalid non-CA certificate (has CA markings)");
+            case V_ERR_PROXY_PATH_LENGTH_EXCEEDED:
+                return("proxy path length constraint exceeded");
+            case V_ERR_KEYUSAGE_NO_DIGITAL_SIGNATURE:
+                return("key usage does not include digital signature");
+            case V_ERR_PROXY_CERTIFICATES_NOT_ALLOWED:
+                return("proxy cerificates not allowed, please set the appropriate flag");
+            case V_ERR_INVALID_EXTENSION:
                 return("invalid or inconsistent certificate extension");
-        case V_ERR_INVALID_POLICY_EXTENSION:
+            case V_ERR_INVALID_POLICY_EXTENSION:
                 return("invalid or inconsistent certificate policy extension");
-        case V_ERR_NO_EXPLICIT_POLICY:
+            case V_ERR_NO_EXPLICIT_POLICY:
                 return("no explicit policy");
+            case V_ERR_APPLICATION_VERIFICATION:
+                return("application verification failure");
         default:
                 return "error number " + error;
         }
