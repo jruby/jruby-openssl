2322e4d2747cf239038d78ec4fc415e64eaf66d3
kares <self@kares.org>
2022-03-02 17:54:17 +0100

    Fix: work-around JRuby 9.2 autoload behavior
    to be able to install jruby-openssl >= 0.12 on JRuby 9.2
    while the default gem (shipped with JRuby) is < 0.12
    
    GH-248

--- lib/jopenssl/load.rb
+++ lib/jopenssl/load.rb
@@ -34,3 +34,48 @@ end
 if RUBY_VERSION > '2.3'
   load 'jopenssl/_compat23.rb'
 end
+
+# NOTE: content bellow should live in *lib/openssl.rb* but due RubyGems/Bundler
+# `autoload :OpenSSL` this will cause issues if an older version (0.11) is the
+# default gem under JRuby 9.2 (which on auto-load does not trigger a dynamic
+# require - this is only fixed in JRuby 9.3)
+
+module OpenSSL
+  autoload :Config, 'openssl/config' unless const_defined?(:Config, false)
+  autoload :PKCS12, 'openssl/pkcs12'
+end
+
+=begin
+= Info
+  'OpenSSL for Ruby 2' project
+  Copyright (C) 2002  Michal Rokos <m.rokos@sh.cvut.cz>
+  All rights reserved.
+
+= Licence
+  This program is licensed under the same licence as Ruby.
+  (See the file 'LICENCE'.)
+=end
+
+require_relative 'openssl/bn'
+require_relative 'openssl/pkey'
+require_relative 'openssl/cipher'
+#require_relative 'openssl/config' if OpenSSL.const_defined?(:Config, false)
+require_relative 'openssl/digest'
+require_relative 'openssl/hmac'
+require_relative 'openssl/x509'
+require_relative 'openssl/ssl'
+require_relative 'openssl/pkcs5'
+
+module OpenSSL
+  # call-seq:
+  #   OpenSSL.secure_compare(string, string) -> boolean
+  #
+  # Constant time memory comparison. Inputs are hashed using SHA-256 to mask
+  # the length of the secret. Returns +true+ if the strings are identical,
+  # +false+ otherwise.
+  def self.secure_compare(a, b)
+    hashed_a = OpenSSL::Digest.digest('SHA256', a)
+    hashed_b = OpenSSL::Digest.digest('SHA256', b)
+    OpenSSL.fixed_length_secure_compare(hashed_a, hashed_b) && a == b
+  end
+end
