491f1eb30dc61a978a9deef5627862117e4f125f
Justin Stoller <justin.stoller@gmail.com>
2023-09-26 21:13:33 -0700

    Replace methods removed in BC 1.75
    Several methods and classes were deprecated in BC 1.70 and many of those
    in the org.bouncycastle.asn1.* were removed in 1.75.
    
    This commit moves away from removed method and classes so BouncyCastle
    can be updated to 1.76.

--- src/main/java/org/jruby/ext/openssl/X509Extension.java
+++ src/main/java/org/jruby/ext/openssl/X509Extension.java
@@ -38,6 +38,7 @@ import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Encoding;
 import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.ASN1IA5String;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1OctetString;
@@ -46,7 +47,6 @@ import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ASN1String;
 import org.bouncycastle.asn1.ASN1TaggedObject;
 import org.bouncycastle.asn1.BERTags;
-import org.bouncycastle.asn1.DERIA5String;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DERUniversalString;
 import org.bouncycastle.asn1.DLSequence;
@@ -620,7 +620,7 @@ public class X509Extension extends RubyObject {
         case GeneralName.uniformResourceIdentifier:
             if ( ! tagged ) out.append('U').append('R').append('I').
                 append(':');
-            val = DERIA5String.getInstance(obj).getString();
+            val = ASN1IA5String.getInstance(obj).getString();
             out.append( ByteList.plain(val) );
             break;
         case GeneralName.directoryName:
