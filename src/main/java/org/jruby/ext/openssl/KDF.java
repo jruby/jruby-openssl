a29a273939765582bdcef6ea189336fe6dd2302e
kares <self@kares.org>
2026-03-07 20:36:58 +0100

    [compat] implement OpenSSL::KDF.hkdf

--- src/main/java/org/jruby/ext/openssl/KDF.java
+++ src/main/java/org/jruby/ext/openssl/KDF.java
@@ -25,6 +25,7 @@ package org.jruby.ext.openssl;
 
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import javax.crypto.Mac;
 
 import org.jruby.*;
 import org.jruby.anno.JRubyMethod;
@@ -50,6 +51,7 @@ public class KDF {
     }
 
     private static final String[] PBKDF2_ARGS = new String[] { "salt", "iterations", "length", "hash" };
+    private static final String[] HKDF_ARGS = new String[] { "salt", "info", "length", "hash" };
 
     @JRubyMethod(module = true) // pbkdf2_hmac(pass, salt:, iterations:, length:, hash:)
     public static IRubyObject pbkdf2_hmac(ThreadContext context, IRubyObject self, IRubyObject pass, IRubyObject opts) {
@@ -61,6 +63,61 @@ public class KDF {
         catch (NoSuchAlgorithmException|InvalidKeyException e) {
             throw newKDFError(context.runtime, e.getMessage());
         }
+    }
+
+    @JRubyMethod(module = true) // hkdf(ikm, salt:, info:, length:, hash:)
+    public static IRubyObject hkdf(ThreadContext context, IRubyObject self, IRubyObject ikm, IRubyObject opts) {
+        IRubyObject[] args = extractKeywordArgs(context, (RubyHash) opts, HKDF_ARGS, 0);
+        try {
+            return hkdfImpl(context.runtime, ikm, args);
+        }
+        catch (NoSuchAlgorithmException|InvalidKeyException e) {
+            throw newKDFError(context.runtime, e.getMessage());
+        }
+    }
+
+    static RubyString hkdfImpl(final Ruby runtime, final IRubyObject ikmArg, final IRubyObject[] args)
+        throws NoSuchAlgorithmException, InvalidKeyException {
+        final byte[] ikm = ikmArg.convertToString().getBytes();
+        final byte[] salt = args[0].convertToString().getBytes();
+        final byte[] info = args[1].convertToString().getBytes();
+
+        final long length = RubyNumeric.num2long(args[2]);
+        if (length < 0) throw runtime.newArgumentError("length must be non-negative");
+
+        final Mac mac = getMac(args[3]);
+        final int macLength = mac.getMacLength();
+        if (length > 255L * macLength) {
+            throw newKDFError(runtime, "length must be <= 255 * HashLen");
+        }
+
+        mac.init(new SimpleSecretKey(mac.getAlgorithm(), salt));
+        final byte[] prk = mac.doFinal(ikm);
+
+        mac.init(new SimpleSecretKey(mac.getAlgorithm(), prk));
+
+        final byte[] okm = new byte[(int) length];
+        byte[] block = new byte[0];
+        int offset = 0;
+
+        for (int i = 1; offset < okm.length; i++) {
+            if (block.length > 0) mac.update(block);
+            if (info.length > 0) mac.update(info);
+            mac.update((byte) i);
+
+            block = mac.doFinal();
+
+            final int copyLength = Math.min(block.length, okm.length - offset);
+            System.arraycopy(block, 0, okm, offset, copyLength);
+            offset += copyLength;
+        }
+
+        return StringHelper.newString(runtime, okm);
+    }
+
+    private static Mac getMac(final IRubyObject digest) throws NoSuchAlgorithmException {
+        final String digestAlg = HMAC.getDigestAlgorithmName(digest);
+        return HMAC.getMacInstance(digestAlg);
     }
 
     static RaiseException newKDFError(Ruby runtime, String message) {
