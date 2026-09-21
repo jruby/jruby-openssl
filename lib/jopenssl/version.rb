86bfbb4324f5865efcad66ef915ccd68e98f2ed2
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-09-22 04:09:07 +0800

    [deps] bump Bouncy Castle to 1.86
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- lib/jopenssl/version.rb
+++ lib/jopenssl/version.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 module JOpenSSL
   VERSION = '0.19.2.dev'
-  BOUNCY_CASTLE_VERSION = '1.85'
+  BOUNCY_CASTLE_VERSION = '1.86'
 
   # @private
   BOUNCY_CASTLE_PATCH_VERSIONS = {
-    'bcprov-jdk18on' => "#{BOUNCY_CASTLE_VERSION}.2"
+    'bctls-jdk18on' => "#{BOUNCY_CASTLE_VERSION}.1",
   }.freeze
   private_constant :BOUNCY_CASTLE_PATCH_VERSIONS
 
